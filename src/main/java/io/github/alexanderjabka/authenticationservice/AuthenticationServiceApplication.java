2581ad2aa3c1fa2762d8cbf067ed9a5e7e4317fe
sasha <popruzhenko.shura@mail.ru>
2025-10-14 10:19:19 +0300

    create base structure of authentication service

---
+++ src/main/java/io/github/alexanderjabka/authenticationservice/AuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.alexanderjabka.authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceApplication.class, args);
+    }
+
+}
