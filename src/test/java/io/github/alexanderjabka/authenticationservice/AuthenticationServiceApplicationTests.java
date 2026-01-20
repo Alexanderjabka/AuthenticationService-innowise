79685d8e1e2674a6893b5e0191261d7f93572545
sasha <popruzhenko.shura@mail.ru>
2026-01-20 13:02:27 +0300

    fix test job in pipeline

--- src/test/java/io/github/alexanderjabka/authenticationservice/AuthenticationServiceApplicationTests.java
+++ src/test/java/io/github/alexanderjabka/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -2,6 +2,7 @@ package io.github.alexanderjabka.authenticationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class AuthenticationServiceApplicationTests {
