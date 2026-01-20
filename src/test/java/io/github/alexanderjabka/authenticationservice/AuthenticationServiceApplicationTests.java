1917f08f8cf392ee2cf4b7f89bdaeb05a0e6e058
sasha <popruzhenko.shura@mail.ru>
2026-01-20 13:22:22 +0300

    create test profile for tests

--- src/test/java/io/github/alexanderjabka/authenticationservice/AuthenticationServiceApplicationTests.java
+++ src/test/java/io/github/alexanderjabka/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -1,11 +1,17 @@
 package io.github.alexanderjabka.authenticationservice;
 
+import io.github.alexanderjabka.authenticationservice.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthenticationServiceApplicationTests {
+
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     void contextLoads() {
